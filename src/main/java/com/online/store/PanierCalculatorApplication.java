6e37b6bc2f15b4450d9413b20d900b3aab900e9f
a-maiza <abdelmaiza33@gmail.com>
2025-11-21 01:49:14 +0100

    create project template

---
+++ src/main/java/com/online/store/PanierCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.online.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PanierCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PanierCalculatorApplication.class, args);
+	}
+
+}
